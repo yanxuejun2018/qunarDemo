00db0b3cd593faa17e4f3c8a5d7b99d45f7903c3
yanxuejun2018 <1181977746@qq.com>
2019-05-01 16:16:58 +0800

    second time

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const resolve = dir => path.join(__dirname, dir);
+
+const BASE_URL = process.env.NODE_ENV === "production" ? "/qunar" : "/";
+
+module.exports = {
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("@c", resolve("src/components"));
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false,
+  //解决跨域
+  devServer: {
+    proxy: "http://localhost:8080"
+  }
+};
